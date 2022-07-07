1a85106fa6ed7106e8b4839b7b4caa2662a5af38
Sharosez <sharosez87@gmail.com>
2022-07-07 12:00:33 +0545

    Added HomePage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -11,15 +11,17 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Body Here')),
-      bottomNavigationBar:
-          BottomNavigationBar(type: BottomNavigationBarType.fixed, items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
-        BottomNavigationBarItem(icon: Icon(Icons.search), label: 'search'),
-        BottomNavigationBarItem(icon: Icon(Icons.video_call), label: 'reels'),
-        BottomNavigationBarItem(icon: Icon(Icons.shop), label: 'shop'),
-        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'account'),
-      ]),
+      body: const Center(child: const Text('Body Here')),
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'search'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.video_call), label: 'reels'),
+            BottomNavigationBarItem(icon: Icon(Icons.shop), label: 'shop'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'account'),
+          ]),
     );
   }
 }
